4f8481273b09b0fa42e982df831f57647150888c
mohitkr2809 <mohitkrmnm282003@gmail.com>
2025-08-14 14:12:39 +0530

    .

---
+++ Chai Aur Code Practice/01_basics/06_nums_and_maths.js
@@ -0,0 +1,46 @@
+const score = 400;
+
+const balance = new Number(100); // new is used for defining a definite variable
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++= Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+
+console.log(Math.random()); // math.random value from 0-1
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+// Below code is very very important
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
+
+
+
+
+
+
+
+
+
+
+
